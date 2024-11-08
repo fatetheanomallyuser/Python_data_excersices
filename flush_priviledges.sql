d9b02facaf171a9c88b5242331bd8e252f514acf
fatetheanomallyuser <dacostabr17@outlook.com>
2024-11-08 16:05:10 -0500

    Update flush_priviledges.sql

--- flush_priviledges.sql
+++ flush_priviledges.sql
@@ -3,8 +3,8 @@ SELECT user, plugin
 FROM mysql.user
 WHERE user='root';
 
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Brazuka17!';
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '#enter password here';
 #OR
-CREATE USER 'web_app_usr'@'localhost' IDENTIFIED WITH mysql_native_password BY 'Brazuka17!';
+CREATE USER 'web_app_usr'@'localhost' IDENTIFIED WITH mysql_native_password BY '#enter password here!';
 
 FLUSH privileges;
